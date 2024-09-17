3e6eb0b99470fa58f5fd0b9531e2599844106d91
midha-abhishek <70281955+midha-abhishek@users.noreply.github.com>
2024-09-16 20:40:25 -0400

    Update advanced_analysis.sql

--- advanced_analysis.sql
+++ advanced_analysis.sql
@@ -1,3 +1,5 @@
+DROP VIEW IF EXISTS ActiveLoansWithLargePayments;
+
 -- Creating a view of active loans with payments greater than $1000:
 
 CREATE VIEW ActiveLoansWithLargePayments AS
@@ -6,6 +8,8 @@ FROM loans AS l
 JOIN loan_payments AS lp
 	ON l.loan_id = lp.loan_id
 WHERE lp.payment_amount > 1000;
+
+SELECT TOP 10 * FROM ActiveLoansWithLargePayments;
 
 -- Creating an index on `transaction_date` in the `transactions` table for performance optimization:
 
